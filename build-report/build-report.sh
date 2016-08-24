e37648bbf6ccaba629ac6c65cdcd642e459ee0a3
Eric Rosenberg <ehaydenr@gmail.com>
2016-08-24 19:45:44 -0400

    update

--- build-report/build-report.sh
+++ build-report/build-report.sh
@@ -2,3 +2,10 @@
 ls -al
 cp resource-analysis/analysis-latest.txt report/report.txt
 echo $'report done!\n' >> report/report.txt
+
+echo '\documentclass{article}' >> report/report.tex
+echo '\begin{document}' >> report/report.tex
+cat report/report.txt >> report/report.tex
+echo '\end{document}'
+
+pdflatex report/report.tex -output-directory report
